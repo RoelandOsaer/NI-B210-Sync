4de0f8c5bca5f323f88726ef8ef233db55ed4e1b
RoelandOsaer <50915941+RoelandOsaer@users.noreply.github.com>
2023-05-10 16:00:03 +0200

    Update main.cpp

--- software/examples/TX/main.cpp
+++ software/examples/TX/main.cpp
@@ -179,8 +179,8 @@ int UHD_SAFE_MAIN(int argc, char *argv[])
        	std::string serial = m["mboard_serial"];
        	std::cout << "Serial number: " << serial << std::endl;
 
-	uhd::stream_args_t stream_args("sc16"); // complex shorts (uint16_t)
-        stream_args.channels = {0};
+	uhd::stream_args_t stream_args(""); // complex shorts (uint16_t)
+        stream_args.channels = {0,1};
         uhd::tx_streamer::sptr tx_stream = usrp->get_tx_stream(stream_args);
 	
        // size_t nsamps_per_buff = tx_stream->get_max_num_samps();
